0d993a06c1d9c6b18597bdcb2e7661377e29f6a1
NightOwlSect <doyouroshi@gmail.com>
2026-06-12 19:52:33 +0000

    add

--- click_turtle.py
+++ click_turtle.py
@@ -6,6 +6,15 @@ window = turtle.Screen()
 
 # Create a visible turtle object
 my_turtle = turtle.Turtle()
+my_turtle.shape("turtle")
+my_turtle.shapesize(4)
+my_turtle.color("#328FCE")
+
+def click_handler(x, y)
+    colors = ["#FF5733", "#33FF57", "#3357FF", "#F333FF", "#33FFF5"]
+    my_turtle.color(random.choice(colors))
+
+my_turtle.onclick(click_handler)
 
 # Keep the window open and listening for events
 window.mainloop()
