8e0b097b51bc4aa85c47e6c9ddc03d27d77ff706
NightOwlSect <doyouroshi@gmail.com>
2026-06-15 17:57:02 +0000

    FUHHHH

--- click_turtle.py
+++ click_turtle.py
@@ -9,11 +9,15 @@ my_turtle = turtle.Turtle()
 my_turtle.shape("turtle")
 my_turtle.shapesize(4)
 my_turtle.color("purple")
+my_turtle.pendown()
 
-def click_handler(x, y):
+def click_handler(x=0, y=0):
     colors = ["#FF5733", "#33FF57", "#ddff33", "#3357FF", "#F333FF", "#33FFF5"]
     my_turtle.color(random.choice(colors))
-    my_turtle.right(45)
+    my_turtle.right(20)
+    my_turtle.forward(50)
+    window.ontimer(click_handler,500)
+
 
 my_turtle.onclick(click_handler)
 
